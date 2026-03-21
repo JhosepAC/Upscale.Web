ae0402d58856a69f71cd2befb9ac5108719f7200
Jhosep Argomedo <jhosepjamil@gmail.com>
2026-03-21 16:59:15 -0500

    feat(Upscale.Web): add login feature with ViewModel and Controller

---
+++ Upscale.Web/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Upscale.Web.Data;
+using Upscale.Web.Models.ViewModels;
+
+namespace Upscale.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Upscale.Web/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Upscale.Web.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El número de documento es requerido")]
+        [Display(Name = "Usuario")]
+        public string DocumentNumber { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
