8e0b6e71d8e6aad5ab01d723cb60e5027c56ebbb
Jhosep Argomedo <jhosepjamil@gmail.com>
2026-03-21 17:11:25 -0500

    feat(Data): add User and UserProfile entities with DbContext configuration

--- Upscale.Web/Data/ApplicationDbContext.cs
+++ Upscale.Web/Data/ApplicationDbContext.cs
@@ -1,6 +1,23 @@
-﻿namespace Upscale.Web.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Upscale.Web.Models.Entities;
+
+namespace Upscale.Web.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map entities to tables
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<UserProfile>().ToTable("UserProfiles");
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
