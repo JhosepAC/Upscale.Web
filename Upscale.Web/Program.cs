79bd44f5fcab32469a973482c60c222262fe7eee
Jhosep Argomedo <jhosepjamil@gmail.com>
2026-03-21 16:55:02 -0500

    feat(Upscale.Web): update program.cs for authentication and SQL Server configuration

--- Upscale.Web/Program.cs
+++ Upscale.Web/Program.cs
@@ -1,29 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Upscale.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// --- SECTION OF SERVICE CONFIGURATION (Dependency Injection) ---
+
 builder.Services.AddControllersWithViews();
+
+// Configuration of the database context with SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// --- SECTION OF MIDDLEWARE CONFIGURATION (HTTP Request Pipeline) ---
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Account}/{action=Login}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
