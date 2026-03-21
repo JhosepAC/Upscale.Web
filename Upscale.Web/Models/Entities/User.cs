3cb258434220af186c2f87c1a6747e5486a07fa3
Jhosep Argomedo <jhosepjamil@gmail.com>
2026-03-21 16:45:48 -0500

    feat(entities): add User and UserProfile entities

---
+++ Upscale.Web/Models/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Upscale.Web.Models.Entities
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string DocumentNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public int FailedAttempts { get; set; } = 0;
+        public bool IsLocked { get; set; } = false;
+        public DateTime? LockoutEnd { get; set; }
+
+        public virtual UserProfile Profile { get; set; }
+    }
+}
---
+++ Upscale.Web/Models/Entities/UserProfile.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Upscale.Web.Models.Entities
+{
+    public class UserProfile
+    {
+        [Key]
+        public int ProfileId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+        public string FirstLastName { get; set; }
+        public string SecondLastName { get; set; }
+        public string DocumentType { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Nationality { get; set; } = "Peruvian";
+        public string Gender { get; set; }
+        public string MobilePhone { get; set; }
+        public string JobTitle { get; set; }
+        public string Organization { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
